8af3490a9925e376851ef58aaaca1367acbcb3e8
heanyang1 <e1124486@u.nus.edu>
2025-08-16 14:17:46 +0800

    "Fix" a linker error on MacOS when the test code doesn't include the library that has customize linker flags.
    The problem is skipped by moving all tests to `eval_test.rs`.

--- tests/eval_test.rs
+++ tests/eval_test.rs
@@ -5,13 +5,14 @@ use relic::parser::Parse;
 use relic::preprocess::PreProcess;
 use relic::runtime::{DbgState, Runtime, RuntimeNode, StackMachine};
 use relic::symbol::Symbol;
-use relic::{RT, file_to_node, rt_pop, rt_start, run_node};
+use relic::{RT, rt_pop, rt_start};
 use serial_test::serial;
 use std::collections::HashMap;
-use std::path::PathBuf;
-use std::process::Command;
-use std::str::FromStr;
 use std::sync::atomic::AtomicUsize;
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
 
 macro_rules! assert_eval_node {
     ($code:expr, $expected:expr) => {{
@@ -694,4 +695,86 @@ fn debug_test() {
         let mut runtime = RT.write().unwrap();
         runtime.clear();
     }
+}
+
+#[test]
+fn test_run_repl() {
+    let mut cmd = Command::new(env!("CARGO_BIN_EXE_relic"))
+        .args(["run", "-i", "examples/interpreter.lisp"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    {
+        let mut stdin = cmd.stdin.take().unwrap();
+        stdin
+            .write_all(
+                br#"
+(define fib
+  (lambda (x)
+    (cond ((= x 0) 0)
+          ((= x 1) 1)
+          ('t (+ (fib (- x 2)) (fib (- x 1)))))))
+(define map
+  (lambda (f l)
+    (cond ((eq? l '()) '())
+          ('t (cons (f (car l))
+                    (map f (cdr l)))))))
+(map fib '(1 2 3 4 5 6 7 8 9 10))
+nil"#,
+            )
+            .unwrap();
+    }
+
+    let out = cmd.wait_with_output().unwrap();
+    assert!(out.status.success());
+    assert_eq!(
+        String::from_utf8(out.stdout).unwrap(),
+        r#"> = nil
+> = nil
+> = (1 1 2 3 5 8 13 21 34 55)
+> result: nil
+"#
+    );
+}
+
+#[test]
+fn test_repl() {
+    let mut cmd = Command::new(env!("CARGO_BIN_EXE_relic"))
+        .args(["repl"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    {
+        let mut stdin = cmd.stdin.take().unwrap();
+        stdin
+            .write_all(
+                br#"
+123
+(+ 2 3 4 5)
+(display "hello\n")
+(define (f x) (* x 6))
+(f 7)
+"#,
+            )
+            .unwrap();
+    }
+
+    let out = cmd.wait_with_output().unwrap();
+    assert!(out.status.success());
+    assert_eq!(
+        String::from_utf8(out.stdout).unwrap(),
+        r#"Relic REPL. Press Ctrl+D or type 'exit' to quit.
+= 123
+= 14
+hello
+= nil
+= nil
+= 42
+CTRL-D
+"#
+    );
 }
